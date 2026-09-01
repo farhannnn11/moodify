6a2c35fd1926f866b2725a56b4770f4238e48fb8
farhannn.11 <mohammadfarhankhan413@gmail.com>
2026-09-01 23:59:31 +0530

    Frontend+Backend integration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const authRouter = require("./routes/auth.route");
 const cors = require("cors");
+const path = require("path")
 const app = express();
 const cookie = require("cookie-parser");
+
 const songRouter = require("./routes/song.routes");
 app.use(cookie());
 app.use(
@@ -13,6 +15,7 @@ app.use(
   }),
 );
 app.use(express.json());
+app.use(express.static("./public"));
 app.use("/api/auth", authRouter);
 app.use("/api/songs", songRouter);
 module.exports = app;
